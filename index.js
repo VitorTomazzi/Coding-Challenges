193b232b43d0b7251fbed4e3ab7fb84fe832be47
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-11-19 11:34:10 -0500

    Your commit message

--- index.js
+++ index.js
@@ -165,3 +165,9 @@ function howManyTimes(num) {
 	let letter = 'a'
 	return 'Ed' + letter.repeat(num) + 'bit'
 }
+
+//Create a function that takes a number (from 1 - 60) and returns a corresponding string of hyphens.
+function Go(num) {
+	let hyphen = "-"
+	return hyphen.repeat(num)
+}
