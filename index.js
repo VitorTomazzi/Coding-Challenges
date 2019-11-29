5df2e9a48e2477971b2d6e9b69c4bae9c6bc4a69
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-11-29 15:42:42 -0500

    daily coding challenges 34253

--- index.js
+++ index.js
@@ -205,3 +205,40 @@ function both(n1, n2) {
 function smallerNum(n1, n2) {
 	return Math.min(n1, n2).toString();
 }
+
+//Create a function that takes a number (from 1 to 12) and returns its corresponding month name as a string. 
+//For example, if you're given 3 as input, your function should return "March", because March is the 3rd month.
+function monthName(num) {
+	return ['January','February','March','April','May','June','July','August','September','October','November','December'][num-1];
+}
+
+function monthName(num) {
+	switch(num){
+		case 1:
+			return "January"
+		case 2:
+			return "February"
+		case 3:
+			return "March"
+		case 4:
+			return "April"
+		case 5:
+			return "May"
+		case 6:
+			return "June"
+		case 7:
+			return "July"
+		case 8:
+			return "August"
+		case 9:
+			return "September"
+		case 10:
+			return "October"
+		case 11:
+			return "November"
+		case 12:
+			return "December"
+		default:
+			return "Error"
+	}
+}
